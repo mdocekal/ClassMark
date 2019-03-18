3fdf48d6e7e5cb3acd3f13a1a40c862192fe4d7d
windionleaf <windionleaf@windionleaf-GP60-2OD>
2019-03-18 09:15:45 +0100

    CEEF initialization.

---
+++ plugins/classifiers/ceef/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+""""
+Created on 4. 3. 2019
+SVM classifier plugin for ClassMark.
+
+:author:     Martin Dočekal
+:contact:    xdocek09@stud.fit.vubtr.cz
+"""
+
+from distutils.core import setup
+
+setup(name='ClassMarkPluginClassifierCEEF',
+    version='1.0dev',
+    description='CEEF classifier plugin for ClassMark.',
+    author='Martin Dočekal',
+    entry_points={'classmark.plugins.classifiers': 'ceef = ceef:CEEF'}
+)
